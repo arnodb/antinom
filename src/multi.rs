25bdfc09062c588504f13c3d3bc2d0a28a5f58cb
Arnaud de Bossoreille <arnaud.debossoreille@gmail.com>
2023-11-21 18:21:59 +0100

    Add many1, many0_count and many1_count

--- src/multi.rs
+++ src/multi.rs
@@ -14,6 +14,20 @@ where
     }
 }
 
+pub fn many1<R, B, F>(mut f: F, max_items: u8) -> impl Generator<R, B>
+where
+    R: AntiNomRng,
+    B: Buffer,
+    F: Generator<R, B>,
+{
+    move |rng: &mut R, buffer: &mut B| {
+        let n = rng.gen_range(1..=max_items);
+        for _ in 1..=n {
+            f.gen(rng, buffer);
+        }
+    }
+}
+
 pub fn many_till<R, B, F, G>(mut f: F, mut g: G, max_items: u8) -> impl Generator<R, B>
 where
     R: AntiNomRng,
@@ -64,4 +78,22 @@ where
             f.gen(rng, buffer);
         }
     }
+}
+
+pub fn many0_count<R, B, F>(f: F, max_items: u8) -> impl Generator<R, B>
+where
+    R: AntiNomRng,
+    B: Buffer,
+    F: Generator<R, B>,
+{
+    many0(f, max_items)
+}
+
+pub fn many1_count<R, B, F>(f: F, max_items: u8) -> impl Generator<R, B>
+where
+    R: AntiNomRng,
+    B: Buffer,
+    F: Generator<R, B>,
+{
+    many1(f, max_items)
 }
